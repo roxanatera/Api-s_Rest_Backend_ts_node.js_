605d9350025632e3ce2af5aeb509b6067d91e346
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-27 14:40:03 -0600

    13

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express' 
 import colors from 'colors'
+import cors, { CorsOptions } from 'cors'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec, { swaggerUiOptions } from './config/swagger'
 import router  from './router'
@@ -12,7 +13,7 @@ export async function connectDB() {
         db.sync()
         // console.log( colors.blue( 'Conexión exitosa a la BD'))
     } catch (error) {
-        // console.log(error)
+        console.log(error)
         console.log( colors.red.bold( 'Hubo un error al conectar a la BD') )
     }
 }
@@ -20,6 +21,18 @@ connectDB()
 
 // Instancia de express
 const server = express()
+
+// Permitir conexiones
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        if(origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        } else {
+            callback(new Error('Error de CORS')) 
+        }
+    }
+}
+server.use(cors(corsOptions))
 
 // Leer datos de formularios
 server.use(express.json())
